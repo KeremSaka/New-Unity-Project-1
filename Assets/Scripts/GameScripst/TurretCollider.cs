ff91ee0b9b8eaa37a940e16868c0f5060699a3d1
KeremSaka <tadikemene@hotmail.de>
2017-10-21 01:28:34 +0200

    collider den turrets hinzugefügt plus script für collider fertig gestellet

--- Assets/Scripts/GameScripst/TurretCollider.cs
+++ Assets/Scripts/GameScripst/TurretCollider.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretCollider : MonoBehaviour {
-	private GameObject targets;
+	private GameObject currentTarget;
+    private int currentTargetId;
 	private int pointer = 0;
 	// Use this for initialization
 	void Start () {
@@ -19,31 +20,90 @@ public class TurretCollider : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "Enemy")
 		{
-			targets = other.gameObject;
-
+			if(currentTarget == null)
+            {
+                currentTarget = other.gameObject;
+                MovePlayer mp = currentTarget.GetComponent<MovePlayer>();
+                currentTargetId = mp.ID;
+            }
+            else
+            {
+                currentTarget = getNearestEnemy(currentTarget, other.gameObject);
+                MovePlayer mp = currentTarget.GetComponent<MovePlayer>();
+                currentTargetId = mp.ID;
+            }
 
 
 		}
 	}
 
-	private void OnTriggerExit(Collider other)
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (currentTarget == null)
+            {
+                currentTarget = other.gameObject;
+                MovePlayer mp = currentTarget.GetComponent<MovePlayer>();
+                currentTargetId = mp.ID;
+            }
+            else
+            {
+                MovePlayer mp = other.gameObject.GetComponent<MovePlayer>();
+
+                if (mp.ID != currentTargetId)
+                {
+                    currentTarget = getNearestEnemy(currentTarget, other.gameObject);
+                    mp = currentTarget.GetComponent<MovePlayer>();
+                    currentTargetId = mp.ID;
+                }
+            }
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
 	{
 		if (other.gameObject.tag == "Enemy")
 		{
-			targets = null;
-
-
-
-		}
+            MovePlayer mp = other.gameObject.GetComponent<MovePlayer>();
+            if(currentTargetId == mp.ID)
+            {
+                currentTarget = null;
+            }
+        }
 	}
 
 
 	public GameObject getTarget()
 	{
-		return targets;
+		return currentTarget;
 	}
 	public void delTarget()
 	{
-		targets = null;
+        currentTarget = null;
 	}
+
+    private GameObject getNearestEnemy(GameObject enemy1, GameObject enemy2)
+    {
+        float tempX = gameObject.transform.position.x - enemy1.transform.position.x;
+        float tempZ = gameObject.transform.position.z - enemy1.transform.position.z;
+        float distance = Mathf.Sqrt( Mathf.Pow(tempX, 2) + Mathf.Pow(tempZ, 2));
+        distance = Mathf.Abs(distance);
+
+        tempX = gameObject.transform.position.x - enemy2.transform.position.x;
+        tempZ = gameObject.transform.position.z - enemy2.transform.position.z;
+        float distance2 = Mathf.Sqrt(Mathf.Pow(tempX, 2) + Mathf.Pow(tempZ, 2));
+        distance = Mathf.Abs(distance2);
+
+        if (distance < distance2)
+        {
+            return enemy1;
+        }
+        else
+        {
+            return enemy2;
+        }
+    }
 }
